763cedc6d3634f7bfdc3cd3027384d258fff348a
Raja Khan <rkhan@raileurope.com>
2017-10-26 13:22:26 -0400

    initial commit

--- src/main/java/com/javabrown/app/App.java
+++ src/main/java/com/javabrown/app/App.java
@@ -1,8 +1,19 @@
 package com.javabrown.app;
- 
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.cli.*;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class App {
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) throws Exception {
+        new Cli(args).parse();
+    }
 }
 
