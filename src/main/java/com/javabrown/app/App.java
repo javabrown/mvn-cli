5f3b5fa997168b6fc9c95dca6317f915b341aa36
Raja Khan <rkhan@raileurope.com>
2017-10-26 12:44:23 -0400

    Initial commit

--- src/main/java/com/javabrown/app/App.java
+++ src/main/java/com/javabrown/app/App.java
@@ -1,30 +1,8 @@
 package com.javabrown.app;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.Arrays;
-
-import com.javabrown.app.KeysI;
-import com.javabrown.app.utils.StringUtils;
-
+ 
 public class App {
   public static void main(String[] args) throws Exception {
-    String browser = KeysI.BROWSER_FF;
-
-    CLI cli = new CLI(args);
-    
-    if(!cli.hasError()) {
-      System.out.printf("Running test in Browser - %s\n", cli.getBrowserName());
-      TestMain test = new TestMain(cli);
-      test.execute();
-    }
-    else{
-      System.out.println(cli.getErrorMsg());
-    }
-    
+    System.out.println("Hello World");
   }
 }
 
